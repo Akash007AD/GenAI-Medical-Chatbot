871062d42bdc9a3663fd67c3f6a9f0eb3562c6ae
Akash Debnath <debnath2004akash@gmail.com>
2025-06-30 16:33:23 +0530

    Folder created Successfully

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = 'Generative AI Project',
+    version='0.0.0',
+    author="Akash Debnath",
+    author_email="debnath2004akash@gmail.com",
+    packages=find_packages(),
+    install_requires = []
+    
+)
